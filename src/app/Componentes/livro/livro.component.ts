246a896eeeba3a8f2e999a2642d685a10f46a4d9
Weslley Melo Franco <weslleyfrc9@gmail.com>
2025-06-14 17:15:09 -0300

    add funciont component book

--- src/app/Componentes/livro/livro.component.ts
+++ src/app/Componentes/livro/livro.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class LivroComponent {
 
+  alternarFavorito() {
+    this.livro.favorito = !this.livro.favorito
+  }
+
   livro = {
     titulo: "Warhammer O emperio",
     autoria: "black library",
